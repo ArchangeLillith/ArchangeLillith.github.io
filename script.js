1829addbb79319efc56d285206ba4dee79d8efe9
ArchangeLillith <90073620+ArchangeLillith@users.noreply.github.com>
2024-02-09 10:31:58 -0500

    Glass cards implimented

--- script.js
+++ script.js
@@ -1,23 +1,30 @@
-document.addEventListener('click', e => {
-    const isDropdownButton = e.target.matches('[data-dropdown-button]');
-    if (!isDropdownButton && e.target.closest('[data-dropdown]') !== null) {
-        console.log('clicking on closest');
-        return;
-    } 
+VanillaTilt.init(document.querySelectorAll(".card"), {
+	max: 25,
+	speed: 800,
+	glare: true,
+	"max-glare": 1,
+});
 
-    let currentDropdown;
-    if (isDropdownButton) {
-        console.log('toggling active');
-        currentDropdown = e.target.closest('[data-dropdown]');
-        currentDropdown.classList.toggle('active');
-    }
+document.addEventListener("click", (e) => {
+	const isDropdownButton = e.target.matches("[data-dropdown-button]");
+	if (!isDropdownButton && e.target.closest("[data-dropdown]") !== null) {
+		console.log("clicking on closest");
+		return;
+	}
 
-    document.querySelectorAll('[data-dropdown].active').forEach(dropdown => {
-        if (dropdown === currentDropdown) {
-            console.log('not removing active on current dropdown')
-            return;
-        }
-        console.log('removing active on not current dropdowns')
-        dropdown.classList.remove('active');
-    })
-})
+	let currentDropdown;
+	if (isDropdownButton) {
+		console.log("toggling active");
+		currentDropdown = e.target.closest("[data-dropdown]");
+		currentDropdown.classList.toggle("active");
+	}
+
+	document.querySelectorAll("[data-dropdown].active").forEach((dropdown) => {
+		if (dropdown === currentDropdown) {
+			console.log("not removing active on current dropdown");
+			return;
+		}
+		console.log("removing active on not current dropdowns");
+		dropdown.classList.remove("active");
+	});
+});
